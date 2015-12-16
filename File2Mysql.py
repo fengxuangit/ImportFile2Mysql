c331472496c8a29cd643d78a590e935ed985897e
fengxuangit <978348306@qq.com>
2015-12-16 19:52:20 +0800

    测试SQLmap文件成功。

--- File2Mysql.py
+++ File2Mysql.py
@@ -6,34 +6,47 @@ import pymysql
 import re
 import sys
 import copy
+import logging
+from os.path import basename
 from optparse import OptionParser 
 
+
+class MysqlImport:
+	def __init__(self, host, user, passwd, db='searcku'):
+		self.conn = pymysql.connect(host=host, user=user,passwd=passwd,db=db)
+		self.cursor = self.conn.cursor()
+
+	def executeInsert(self, sql):
+		try:
+			self.cursor.execute(sql)
+		except:
+			logging.critical("sql execute error")
+			sys.exit()
+		self.conn.commit()
+		logging.info("execute sql success")
+
+	def __del__(self):
+		self.cursor.close()
+		self.conn.close()
 
 class File2Mysql:
 	emailreg = re.compile('[\w_]+@([\w]+\.[\w]+)')
 	passreg = re.compile('[\w]{32}|[\w]{40}|[\w]{16}')
 	ipreg = re.compile('([\d]{1,3}\.[\d]{1,3}\.[\d]{1,3}\.[\d]{1,3})')
 	realname = re.compile(u'[\u4e00-\u9fa5]+')
+	conn = MysqlImport(host='virtual.ub',user='mysql', passwd='mysql123', db='searchku')
 
-	def usage(self):
-		parser = OptionParser()  
-		parser.add_option("-f", "--file", dest="filename", help="Import Mysql in File") 
-		parser.add_option("-p", "--type", dest="type", help="assign File type ", default="other")
-		parser.add_option("-c", "--column", dest="column", help="assign column deal file")
-		parser.add_option("-t", "--thread", dest="thread", help="The thread number to run", default=5)
-		parser.add_option("-n", "--name", dest="name", help="table name to save", default=5)
-		global options
-		(options, args) = parser.parse_args() 
-		if len(sys.argv) < 2:
-			print parser.print_help() 
-			sys.exit() 
+	def columns(self,files):
+		with open(files) as f:
+			for line in f.readlines():
+				data =  line.split(options.split)
+				break
+		return data
 
-
-	def dealfile(self, files, table):
-		conn = MysqlImport(host='192.168.108.130',user='mysql', passwd='mysql123', db='searchku')
-		conn.executeInsert(writestruct(table)) # 建表
-
-		formt = Analysis(files)
+	def MixFile(self, files, table):
+		self.conn.executeInsert(writestruct(table)) # 建表
+		logging.info("CREATE table  success")
+		formt = self.Analysis(files)
 		prefix = "insert into {0}(".format(table)
 		suffix = "("
 		for key in sorted(formt):
@@ -50,8 +63,8 @@ class File2Mysql:
 					temp.pop(int(pos))
 				data = ReplaceAll(str(temp))
 				oneline = "{0} {1}\"{2}\")".format(prefix, value, data)	 #处理好sql语句 准备写入文件
-				conn.executeInsert(oneline)
-
+				self.conn.executeInsert(oneline)
+		logging.info("done!!!")
 
 	def Analysis(self, files):
 		i = 0
@@ -92,7 +105,8 @@ class File2Mysql:
 		return sql
 
 
-	def writetable(self, table, data):
+	def SqlmapFile(self,files, table):
+		data = self.columns(files)
 		sql = '''CREATE TABLE IF NOT EXISTS `{0}` (`id` int(11) NOT NULL AUTO_INCREMENT,'''.format(table)
 		for line in data:
 			if line == 'id':
@@ -102,10 +116,12 @@ class File2Mysql:
 	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 		'''.strip()
 		sql = "{0}{1}".format(sql, suffix)
-		print sql
-		# return sql
+		self.conn.executeInsert(sql) # TODO 打印信息
+		logging.info("CREATE table  success")
+		self.insertsql(files, table)
+		logging.info("done!!!")
 
-	def insertsql(self, table):
+	def insertsql(self,files, table):
 		sql = "insert into `{0}`(".format(table)
 		temp = ""
 		i = idpos = 0  # 在这里为1是判断是否为第一行
@@ -114,37 +130,63 @@ class File2Mysql:
 				if line == "\n":  #遇到空行pass
 					continue
 				temp = "{0}) values(".format(sql[:-1]) #这里是 insert into table(asda,asd,asd) values(
-				for value in line.split(sp):
+				for value in line.split(options.split):
 					if value == 'id': #如何 列中有ID的话 pass 因为我们自己会生成ID并自增
 						idpos = i  #纪录ID的位置 
+						logging.debug("found id and remember this position")
 						continue
 					if i > 0:
-						if value == line.split(sp)[idpos]:  #如何这个值等于ID位置的值的话就pass
+						if value == line.split(options.split)[idpos]:  #如何这个值等于ID位置的值的话就pass
+							logging.warning("drop the `id` line ")
 							continue
 						temp += "\"{0}\",".format(value.replace('\n','')) #把值排好
 					else:
 						sql += "`{0}`,".format(value.replace('\n',''))  #第一次循环的时候 把columns排好
 				if i >0 :
 					tmp = "{0})".format(temp[:-1]) #最后闭合括号
-					print tmp  #mysql import 
+					self.conn.executeInsert(tmp)  #mysql import todo 打印信息
 				i += 1
+
+	def usage(self):
+		parser = OptionParser()  
+		parser.add_option("-f", "--file", dest="file", help="Import Mysql in File") 
+		parser.add_option("-p", "--type", dest="type", help="assign File type ", default="other")
+		parser.add_option("-c", "--column", dest="column", help="assign column deal file")
+		parser.add_option("-t", "--thread", dest="thread", help="The thread number to run", default=5)
+		parser.add_option("-n", "--name", dest="name", help="table name to save")
+		parser.add_option("-s", "--split", dest="split", help="the split symbol", default=",")
+		parser.add_option("-l", "--log", dest="logfile", help="save log file path", default="File2Mysql.log")
+		global options
+		(options, args) = parser.parse_args() 
+		if len(sys.argv) < 2:
+			print parser.print_help() 
+			sys.exit()
+
+	def logformat(self):
+		if options.logfile == None:
+			options.logfile = options.name[:options.name.find('.')] + '.log'
+		logging.basicConfig(level=logging.INFO, format='%(asctime)s %(filename)s %(levelname)s %(message)s',
+                datefmt='%a, %d %b %Y %H:%M:%S',
+                filename=options.logfile,
+                filemode='w')
+		console = logging.StreamHandler()
+		console.setLevel(logging.INFO)
+		formatter = logging.Formatter('%(asctime)s %(filename)s %(levelname)s %(message)s')
+		console.setFormatter(formatter)
+		logging.getLogger('').addHandler(console)
 
 	def main(self):
 		self.usage()
-	
-class MysqlImport:
-	def __init__(self, host, user, passwd, db='searcku'):
-		self.conn = pymysql.connect(host=host, user=user,passwd=passwd,db=db)
-		self.cursor = self.conn.cursor()
-
-	def executeInsert(self, sql):
-		self.cursor.execute(sql)
-		self.conn.commit()
-
-	def __del__(self):
-		print 'close'
-		self.cursor.close()
-		self.conn.close()
+		self.logformat()
+		logging.info("start handle with {0} ".format(options.name))
+		if options.name == None:
+			name = basename(options.file[:options.file.find('.')])
+		else:
+			name = options.name
+		if options.type == 'other':
+			self.MixFile(options.file, name)
+		else:
+			self.SqlmapFile(options.file, name)
 
 if __name__ == '__main__':
 	a = File2Mysql()
